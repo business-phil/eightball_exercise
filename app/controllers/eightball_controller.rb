2b33efd3966446b636507f65ed33b7fd50687879
Phil <pfb3cn@virginia.edu>
2017-02-03 15:41:50 -0500

    added eight ball functionality

--- app/controllers/eightball_controller.rb
+++ app/controllers/eightball_controller.rb
@@ -1,4 +1,12 @@
 class EightballController < ApplicationController
   def index
+    if session[:answer]
+      @answer = Answer.find(session[:answer])
+    end
+  end
+
+  def refresh
+    session[:answer] = Random.rand(1..20)
+    redirect_to "/eightball"
   end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,5 @@
 class Answer < ApplicationRecord
+  validates :answer, presence:true
+  validates :category, presence:true, inclusion: { in: %w(positive negative non-committal),
+    message: "Answer must be positive, negative, or non-committal" }
 end
